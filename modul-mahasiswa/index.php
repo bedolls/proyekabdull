cc2eedf3c0d487e4eb4409ed2044ce9fd8e9822f
A <147518138+bedolls@users.noreply.github.com>
2024-12-18 16:28:01 +0700

    mngubah di hapus dan index

--- modul-mahasiswa/index.php
+++ modul-mahasiswa/index.php
@@ -58,13 +58,67 @@
                         <td><?=$data['jurusan']?></td>
                         <td><?=$data['nm_dos']?></td>
                         <td>
+
+                        <!-- TOMBOL HAPUS-->
+                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#detail<?=$data['nim']?>">
+                            <i class="fa-solid fa-eye"></i>
+                            </button>
+
+                            <!-- MODAL HAPUS-->
+                            <div class="modal fade" id="detail<?=$data['nim']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Data <?=$data['nm_mhs']?> </h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <img width="200" src="foto/<?=$data['foto']?>" alt="">
+                                    <table class="table">
+                                            <tr>
+                                            <td scope="col">NIM</td>
+                                            <th scope="col">:<?=$data['nim']?></th>
+                                            </tr>
+                                            <tr>
+                                            <td scope="col">Nama Lengkap</td>
+                                            <th scope="col">:<?=$data['nm_mhs']?></th>
+                                            </tr>
+                                            <tr>
+                                            <td scope="col">Tempat Lahir</td>
+                                            <th scope="col">:<?=$data['tmp_lahir']?></th>
+                                            </tr>
+                                            <tr>
+                                            <td scope="col">Tanggal Lahir</td>
+                                            <th scope="col">:<?=$data['tgl_lahir']?></th>
+                                            </tr>
+                                            <tr>
+                                            <td scope="col">Alamat</td>
+                                            <th scope="col">:<?=$data['alamat']?></th>
+                                            </tr>
+                                            <tr>
+                                            <td scope="col">Email</td>
+                                            <th scope="col">:<?=$data['email']?></th>
+                                </tr> 
+                                </table>           
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <!-- TOMBOL EDIT -->
+
+
                             <a class="btn btn-info btn-sm" href="edit.php?id=<?=$data['id']?>"><i class="fa fa-pen-to-square"></i></a>
                             
                             <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus<?=$data['id']?>">
                             <i class="fa-solid fa-trash"></i>
                             </button>
 
-                            <!-- Modal -->
+                            <!-- Modal Hapus! -->
                             <div class="modal fade" id="hapus<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
@@ -73,11 +127,11 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    Yakin data <b><?=$data['nama ']?></b> ingin dihapus?
+                                    Yakin data <b><?=$data['nama']?></b> ingin dihapus?
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                    <a href="hapus.php?xyz=<?=$data['id']?>" class="btn btn-danger">Hapus</a>
+                                    <a href="hapus.php?xyz=<?=$data['nim']?>" class="btn btn-danger">Hapus</a>
                                 </div>
                                 </div>
                             </div>
--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
           <a class="nav-link active" aria-current="page" href="/is62/PROJECT/index.php">Beranda</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Mahasiswa</a>
+          <a class="nav-link active" aria-current="page" href="/proyekabdul/modul-mahasiswa/index.php">Data Mahasiswa</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/proyekabdul/modul-jurusan/index.php">Data Jurusan</a>
