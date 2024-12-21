bd4c02c17f9a91b546cb63ab070286505e1cb4aa
A <147518138+bedolls@users.noreply.github.com>
2024-12-21 11:12:32 +0700

    menambahkan log out dan merubah isi login

--- navbar.php
+++ navbar.php
@@ -18,6 +18,10 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/proyekabdul/modul-dosen/index.php">Data Dosen</a>
         </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/proyekabdul/logout.php">Log Out</a>
+        </li>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
